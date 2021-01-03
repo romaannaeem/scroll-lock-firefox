50e2d504cab378fcfda37a0e65493cb494eb520c
Romaan <romaan567@hotmail.com>
2021-01-03 17:27:15 -0500

    small fix

--- popup/toggle_lock.js
+++ popup/toggle_lock.js
@@ -46,7 +46,9 @@ function listenForClicks() {
 function reportExecuteScriptError(error) {
   document.querySelector('#popup-content').classList.add('hidden');
   document.querySelector('#error-content').classList.remove('hidden');
-  console.error(`Failed to execute beastify content script: ${error.message}`);
+  console.error(
+    `Failed to execute scroll lock content script: ${error.message}`
+  );
 }
 
 browser.tabs
